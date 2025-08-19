b8501767967c180e78a74e7f73c06031f42638c7
babar nawaz <nawazbabar398@gmail.com>
2025-08-19 18:52:30 +0500

    nested_Routers
    I practice the nested if Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,29 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 import Home from "./assets/home";
 import About from "./assets/About";
 import Contact from "./assets/Contact";
 import NavBar from "./NavBar";
 import NotFound from "./NotFound";
+import Dashboard from "./Dashboard";
+import Profile from "./Profile";
+import Settings from "./Settings";
+
 export default function App() {
   return (
-    <Router>
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <div>
+      <Router>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Dashboard" element={<Dashboard />}>
+            <Route path="profile" element={<Profile />} />
+            <Route path="settings" element={<Settings />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </div>
   );
 }
---
+++ src/Settings.jsx
@@ -0,0 +1,4 @@
+function Settings() {
+  return <h2>User Settings</h2>;
+}
+export default Settings;
